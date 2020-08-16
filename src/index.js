829c5e74b43134c2ed9bbcb0bcc2fa749d820cd7
Feber <cochabambinoski@gmail.com>
2020-08-16 11:58:31 -0400

    fixing some files and adding bookarray

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  App from './components/App';
+import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import bookReducer from './reducers/book';
+
+const bookArray = [
+  {
+    id:1,
+    title:'BookOne',
+    category:'Action'
+  },
+  {
+    id:2,
+    title:'BookTwo',
+    category:'Horror'
+  }
+];
+
+export const initialState = {
+  books:bookArray
+}
+
+const store = createStore(bookReducer);
+console.log(store.getState());
 
 ReactDOM.render(
-  <App />,
+  <Provider store = {store}><App/></Provider>,
   document.getElementById('root'),
 );
 
--- src/reducers/book.js
+++ src/reducers/book.js
@@ -3,10 +3,12 @@
  * with CREATE_BOOK and REMOVE_BOOK actions.Remember,
  *  your state can not be mutated!
  */
+import {initialState} from '../index';
+import { ADD_BOOK, REMOVE_BOOK } from '../actions/index';
 
-const {
+/*const {
   ADD_BOOK
-} = require("../src/actions");
+} = require("../src/actions");*/
 
 export default function bookReducer(state = initialState, action) {
   switch (action.type) {
