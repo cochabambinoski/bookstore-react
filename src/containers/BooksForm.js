2aa0010f0975b46fb6548a75b326d85615a59c4f
Feber <cochabambinoski@gmail.com>
2020-08-18 20:26:09 -0400

    cleaning Handle Submit

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createBook } from '../actions';
 import { randomId } from '../index';
+
 class BooksForm extends React.Component {
   constructor(props) {
     super(props);
@@ -13,54 +14,66 @@ class BooksForm extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleChange(event) {
-    if(event.target.tagName === 'INPUT') {
+    if (event.target.tagName === 'INPUT') {
       this.setState({
-        title: event.target.value
-      })
-    }else {
+        title: event.target.value,
+      });
+    } else {
       this.setState({
-        category: event.target.value
-      })
+        category: event.target.value,
+      });
     }
   }
+
   handleSubmit(event) {
     event.preventDefault();
     this.setState({
-      id: randomId()
-    })
-    this.props.addBook(this.state)
+      id: randomId(),
+    });
+    if (this.state.title != ''){
+      this.props.addBook(this.state); }
     // console.log('book added')
+    this.setState({
+      title: '',
+      category: '',
+    });
   }
+
   render() {
     const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
     return (
       <div>
         <form>
-          <input onChange = {this.handleChange} type="text" placeholder="Enter a new book" />
-          <select onChange = {this.handleChange}>
+          <input value={this.state.title} onChange={this.handleChange} type="text" placeholder="Enter a new book" />
+          <select onChange={this.handleChange}>
             {
             // eslint-disable-next-line max-len
             bookCategories.map(category => <option key={category} value={category}>{category}</option>)
           }
           </select>
-          <button onClick = {this.handleSubmit} type="submit">Save Book</button>
+          <button onClick={this.handleSubmit} type="submit">Save Book</button>
         </form>
-        <h1>input: {this.state.title}</h1>
-        <h1>category: {this.state.category}</h1>
+        <h1>
+          input:
+          {this.state.title}
+        </h1>
+        <h1>
+          category:
+          {this.state.category}
+        </h1>
       </div>
     );
   }
 }
 // const mapState
-const mapDispatchToProps = dispatch => {
-  return {
-    addBook: book => dispatch(createBook(book)),
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  addBook: book => dispatch(createBook(book)),
+});
 
-export default connect(null,mapDispatchToProps)(BooksForm);
+export default connect(null, mapDispatchToProps)(BooksForm);
 
 /**
  * The component should store the title and the book category in its own state.
