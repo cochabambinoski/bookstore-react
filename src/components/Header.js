be73cc9e634dc5ea33ce69b4d737626081eccabf
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-08-24 17:29:11 -0400

    Add the header component and styled it

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+                <a href="#" className="logo">Bookstore CMS</a>
+                <a href="#" className="header-link">Books</a>
+                <a href="#" className="header-link">Categories</a>
+        </header>
+    )
+}
+
+export default Header
